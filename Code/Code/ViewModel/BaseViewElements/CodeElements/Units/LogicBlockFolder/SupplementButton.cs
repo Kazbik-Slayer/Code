f1e07ece93bc1c4a42be16064891caa260c42d98
Баян Шонходоев <bayanshonhodoev@mail.ru>
2022-05-20 15:13:05 +0700

    Array has compilation

--- Code/Code/ViewModel/BaseViewElements/CodeElements/Units/LogicBlockFolder/SupplementButton.cs
+++ Code/Code/ViewModel/BaseViewElements/CodeElements/Units/LogicBlockFolder/SupplementButton.cs
@@ -51,7 +51,7 @@ namespace App2.ViewModel.BaseViewElements.CodeElements.Units.LogicBlockFolder
                     Buttons.Children.Add(new LogicButton(MainField, stackLayout, codeBlock, Button).GetView());
                     Buttons.Children.Add(new WhileCycleButton(MainField, stackLayout, codeBlock, Button).GetView());
                     Buttons.Children.Add(new VariableButton(MainField, stackLayout, codeBlock, Button).GetView());
-                    Buttons.Children.Add(new ArrayButton(MainField, stackLayout, Button).GetView());
+                    Buttons.Children.Add(new ArrayButton(MainField, stackLayout, codeBlock, Button).GetView());
                     Buttons.Children.Add(new AssignmentButton(MainField, stackLayout, codeBlock, Button).GetView());
                     Buttons.Children.Add(new OutputButton(MainField, stackLayout, codeBlock, Button).GetView());
                     Buttons.Children.Add(new CancelButton(MainField).GetView());
--- Code/Code/ViewModel/MainPageElements/MainLayoutElements/Buttons/SelectionButtons/ArrayButton.cs
+++ Code/Code/ViewModel/MainPageElements/MainLayoutElements/Buttons/SelectionButtons/ArrayButton.cs
@@ -1,5 +1,6 @@
 ﻿using App2.ViewModel.BaseViewElements;
 using App2.ViewModel.BaseViewElements.CodeElements.Units;
+using Codeblock.Model;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,13 +11,13 @@ namespace App2.ViewModel.MainPageElenents.MainLayoutElements.Buttons.SelectionBu
     public class ArrayButton : BaseView
     {
         Button Button;
-        public ArrayButton(MainField mainField, StackLayout layoutToAddUnit, Button replaceButton = null)
+        public ArrayButton(MainField mainField, StackLayout layoutToAddUnit, CodeBlock codeBlock, Button replaceButton = null)
         {
             Button = (Button)new SelectionButton("Array", new Color(255 / 256.0, 0 / 256.0, 81 / 256.0)).GetView();
             Button.Clicked += (sender, e) =>
             {
                 if (replaceButton != null) layoutToAddUnit.Children.Remove(replaceButton);
-                layoutToAddUnit.Children.Add(new ArrayView(mainField, layoutToAddUnit).GetView());
+                layoutToAddUnit.Children.Add(new ArrayView(mainField, layoutToAddUnit, codeBlock).GetView());
                 if (replaceButton != null) layoutToAddUnit.Children.Add(replaceButton);
                 mainField.SetCodeField();
             };
